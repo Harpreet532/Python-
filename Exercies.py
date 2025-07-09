d9d5cf62cd3e57b1f3936024e16913d6cd9bb80c
Harpreet Singh <harpreet.canadaucw@gmail.com>
2025-07-08 22:39:22 -0700

    Task 1 Creating variables

---
+++ Exercies.py
@@ -0,0 +1,22 @@
+from tkinter.messagebox import YESNO
+
+name="Harpreet Singh"
+age=36
+height=1.83 # in meters
+is_student=True
+
+print("Name:",name)
+print("Age:",age)
+print("Height:",height)
+print("Is student",is_student)
+
+#Task 1:Creat variables for your city(string),number of kids (integer) and has_pet (boolean)
+city="Abbotsford"
+num_kids=1
+has_pet=False
+
+print("\nYour City:",city)
+print("Number of kids:",num_kids)
+print("Has pet:",has_pet)
+
+#Task 2: Reassigning Variables
